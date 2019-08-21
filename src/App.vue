3b0152eb04f43f937ac8ccc7a54674dc9471b190
zhuxueqiang <2540178359@qq.com>
2019-08-21 09:49:08 +0800

    上传文件

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div>
+      <ul>
+        <li>
+          <router-link to="/">首页</router-link>
+        </li>
+        <li>
+          <router-link to="/head">企业案例</router-link>
+        </li>
+        <li>
+          <router-link to="/head">技术能力</router-link>
+        </li>
+        <li>
+          <router-link to="/head">关于我们</router-link>
+        </li>
+      </ul>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {}
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+ul {
+  padding: 0;
+}
+ul li {
+  list-style: none;
+  padding: 0px;
+}
+a {
+  text-decoration: none;
+  padding: 0px;
+}
+</style>
